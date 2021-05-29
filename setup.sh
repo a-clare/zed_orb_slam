4d57109516fe888f8703815f630e35af1c59fb45
Adam The Canadian <adam.jarrett.clare@gmail.com>
2021-05-29 14:49:52 -0600

    Adding g20 to build and untar or vocabulary

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ sudo apt install libgl1-mesa-dev
 sudo apt install libglew-dev
 
 sudo apt install libpython2.7-dev
+
+git submodule update --init --recursive
+
+cd ORB_SLAM3/Vocabulary
+tar -xf ORBvoc.txt.tar.gz
